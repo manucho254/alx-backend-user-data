87f7c03499a498d0a0cc57785b95e1c3c5d5f88a
manucho254 <manuchoadero@gmail.com>
2024-02-13 20:41:55 +0000

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic auth class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """get Base64 part of the Authorization
         header for a Basic Authentication
         """
