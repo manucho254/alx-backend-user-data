d4137843112466afcebcc7ed692635aca07ef6a1
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-13 23:50:01 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic auth module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,18 @@ class BasicAuth(Auth):
             return
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Basic - Base64 decode"""
+
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return
+        
+        try: 
+            return base64.b64decode(base64_authorization_header).decode("utf-8")
+        except Exception as e:
+            return
