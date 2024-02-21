6e32acb338a6f23edadf3d565f4f0c91506742d1
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-21 15:49:06 +0300

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,5 +70,19 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route("/reset_password", methods="POST", strict_slashes=False)
+def get_reset_password_token():
+    """get reset password token"""
+
+    email = request.form.get("email")
+
+    try:
+        token = get_reset_password_token(email)
+        res = {"email": email, "reset_token": token}
+        return jsonify(res), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
